c3917a9a19839382efae13fb89e3fc7065373ac2
HollyT85 <h.e.turner@gmail.com>
2024-01-31 15:02:47 +0000

    .

--- fureverloved/settings.py
+++ fureverloved/settings.py
@@ -94,6 +94,7 @@ CSRF_TRUSTED_ORIGINS = [
     'https://8000-hollyt85-fureverloved-tnzu4vrihgw.ws-eu107.gitpod.io/*'
 ]
 
+
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
